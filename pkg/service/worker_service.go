3c120ac64c19b4d2f3f19d701b1081c2f5992498
Nghia Duong <minhnghia.duong@amadeus.com>
2021-05-20 22:29:47 +0200

    implement StartJob rpc

--- pkg/service/worker_service.go
+++ pkg/service/worker_service.go
@@ -5,18 +5,40 @@ import (
 	"fmt"
 
 	"github.com/MinhNghiaD/jobworker/api/worker/proto"
+	"github.com/MinhNghiaD/jobworker/pkg/job"
 )
 
 type WorkerService struct {
 	proto.UnimplementedWorkerServiceServer
+	jobsManager job.JobsManager
 }
 
 func NewWorkerService() (*WorkerService, error) {
-	return &WorkerService{}, nil
+	jobsManager, err := job.NewManager()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &WorkerService{
+		jobsManager: jobsManager,
+	}, nil
 }
 
 func (service *WorkerService) StartJob(ctx context.Context, cmd *proto.Command) (*proto.Job, error) {
-	return nil, fmt.Errorf("Unimplemented")
+	if service.jobsManager == nil {
+		return nil, fmt.Errorf("Job Managers is not ready")
+	}
+
+	jobID, err := service.jobsManager.CreateJob(cmd.Cmd, cmd.Args)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.Job{
+		Id: jobID,
+	}, nil
 }
 
 func (service *WorkerService) StopJob(ctx context.Context, request *proto.StopRequest) (*proto.JobStatus, error) {
