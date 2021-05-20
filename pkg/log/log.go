51bbac05d713e90dbf551cd61eb94f17cae58c4c
Nghia Duong <minhnghia.duong@amadeus.com>
2021-05-20 04:55:52 +0200

    implement file logging

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,7 +1,9 @@
 package log
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/sirupsen/logrus"
 )
@@ -12,6 +14,17 @@ type Logger struct {
 }
 
 func (logger *Logger) Close() error {
+	if err := logger.file.Sync(); err != nil {
+		logrus.Errorf("Fail to sync file %s, error: %s", logger.file.Name(), err)
+		return err
+	}
+
+	if err := logger.file.Close(); err != nil {
+		logrus.Errorf("Fail to close file %s, error: %s", logger.file.Name(), err)
+		return err
+	}
+
+	logrus.Debugf("Close logger %s", logger.file.Name())
 	return nil
 }
 
@@ -20,13 +33,54 @@ type LogsManager struct {
 }
 
 func NewManager() (*LogsManager, error) {
-	return &LogsManager{}, nil
+	// TODO use config file to configure logs directory
+	logsDir, err := os.MkdirTemp("", "worker-logs-*")
+	if err != nil {
+		return nil, err
+	}
+
+	logrus.Infof("Log files are stored in %s", logsDir)
+
+	return &LogsManager{
+		logsDir: logsDir,
+	}, nil
 }
 
 func (manager *LogsManager) NewLogger(ID string) (*Logger, error) {
-	return &Logger{}, nil
+	if len(ID) == 0 {
+		return nil, fmt.Errorf("ID cannot be empty")
+	}
+
+	formatter := new(logrus.JSONFormatter)
+	formatter.TimestampFormat = "02-25-2001 15:04:05"
+
+	fileName := filepath.Join(manager.logsDir, fmt.Sprintf("%s.log", ID))
+
+	// Create write only log file.
+	file, err := os.OpenFile(fileName, (os.O_CREATE | os.O_APPEND | os.O_WRONLY), 0644)
+
+	if err != nil {
+		// Cannot open log file.
+		return nil, err
+	}
+
+	logger := logrus.Logger{
+		Out:       file,
+		Formatter: formatter,
+		Hooks:     make(logrus.LevelHooks),
+		Level:     logrus.InfoLevel,
+	}
+
+	return &Logger{
+		file:  file,
+		Entry: logger.WithField("source", "log"),
+	}, nil
 }
 
 func (manager *LogsManager) Cleanup() error {
+	if err := os.RemoveAll(manager.logsDir); err != nil {
+		logrus.Infof("Failed to remove %q: %v", manager.logsDir, err)
+	}
+
 	return nil
 }
