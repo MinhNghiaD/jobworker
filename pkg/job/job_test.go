a11921e3432e81c4599f8568b144032193750e60
Nghia Duong <minhnghia.duong@amadeus.com>
2021-05-20 14:35:28 +0200

    add query job status test

--- pkg/job/job_test.go
+++ pkg/job/job_test.go
@@ -1,7 +1,10 @@
 package job_test
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/MinhNghiaD/jobworker/pkg/job"
 )
@@ -20,6 +23,8 @@ func TestStartJobs(t *testing.T) {
 		return err
 	}
 
+	// TODO: add more test cases and reinforce the behaviour of the command execution.
+	// NOTE: For now, we accept that user can start job with high priviledge. The job will be started but the execution will fail.
 	testcases := []struct {
 		name      string
 		cmd       string
@@ -59,7 +64,7 @@ func TestStartJobs(t *testing.T) {
 		{
 			"top",
 			"top",
-			[]string{"-c"},
+			[]string{"-b"},
 			false,
 		},
 		{
@@ -86,4 +91,146 @@ func TestStartJobs(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestGetJobStatus(t *testing.T) {
+	manager, err := job.NewManager()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// checker
+	checkStatusJob := func(cmd string, args []string, expectedStatus *job.ProcStat) error {
+		j, err := manager.CreateJob(cmd, args)
+
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(time.Second)
+
+		status := j.Status()
+
+		if (status.Stat != expectedStatus.Stat) || (status.ExitCode != expectedStatus.ExitCode) {
+			return fmt.Errorf("Status is %v, when expected %v", status, expectedStatus)
+		}
+
+		return err
+	}
+
+	// TODO: add more test cases and reinforce the behaviour of the command execution.
+	// NOTE: For now, we accept that user can start job with high priviledge. The job will be started but the execution will fail.
+	testcases := []struct {
+		name       string
+		cmd        string
+		args       []string
+		expectStat *job.ProcStat
+		expectErr  bool
+	}{
+		{
+			"Empty Command",
+			"",
+			[]string{},
+			&job.ProcStat{
+				Stat:     job.STOPPED,
+				ExitCode: -1,
+			},
+			true,
+		},
+		{
+			"Non exist command",
+			"abc",
+			[]string{},
+			&job.ProcStat{
+				Stat:     job.STOPPED,
+				ExitCode: -1,
+			},
+			true,
+		},
+		{
+			"ls",
+			"ls",
+			[]string{"-la"},
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 0,
+			},
+			false,
+		},
+		{
+			"mkdir",
+			"mkdir",
+			[]string{fmt.Sprintf("/tmp/%s", randomString(3))},
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 0,
+			},
+			false,
+		},
+		{
+			"whoami",
+			"whoami",
+			[]string{},
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 0,
+			},
+			false,
+		},
+		{
+			"top",
+			"top",
+			[]string{"-b"},
+			&job.ProcStat{
+				Stat:     job.RUNNING,
+				ExitCode: -1,
+			},
+			false,
+		},
+		{
+			"High priviledge",
+			"apt",
+			[]string{"update"},
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 100,
+			},
+			false,
+		},
+		{
+			"sudo",
+			"sudo",
+			[]string{"apt", "update"},
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 1,
+			},
+			false,
+		},
+	}
+
+	for _, testCase := range testcases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := checkStatusJob(testCase.cmd, testCase.args, testCase.expectStat)
+
+			if (err != nil) != (testCase.expectErr) {
+				t.Errorf("Test case %s with input %s returns error %v, while %v error is expected", testCase.name, testCase.cmd, err, testCase.expectErr)
+				return
+			}
+		})
+	}
+}
+
+func randomString(length int) string {
+	var charset string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+
+	return string(b)
 }
