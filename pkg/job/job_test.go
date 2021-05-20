22a7db1fae14ee34e8ba53ab57db677408973646
Nghia Duong <minhnghia.duong@amadeus.com>
2021-05-20 15:21:11 +0200

    add stop jobs test

--- pkg/job/job_test.go
+++ pkg/job/job_test.go
@@ -62,7 +62,7 @@ func TestStartJobs(t *testing.T) {
 			false,
 		},
 		{
-			"top",
+			"long running",
 			"top",
 			[]string{"-b"},
 			false,
@@ -77,6 +77,12 @@ func TestStartJobs(t *testing.T) {
 			"sudo",
 			"sudo",
 			[]string{"apt", "update"},
+			false,
+		},
+		{
+			"bad args",
+			"ls",
+			[]string{"-wrong"},
 			false,
 		},
 	}
@@ -119,8 +125,6 @@ func TestGetJobStatus(t *testing.T) {
 		return err
 	}
 
-	// TODO: add more test cases and reinforce the behaviour of the command execution.
-	// NOTE: For now, we accept that user can start job with high priviledge. The job will be started but the execution will fail.
 	testcases := []struct {
 		name       string
 		cmd        string
@@ -179,7 +183,7 @@ func TestGetJobStatus(t *testing.T) {
 			false,
 		},
 		{
-			"top",
+			"long running",
 			"top",
 			[]string{"-b"},
 			&job.ProcStat{
@@ -208,11 +212,183 @@ func TestGetJobStatus(t *testing.T) {
 			},
 			false,
 		},
+		{
+			"bad args",
+			"ls",
+			[]string{"-wrong"},
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 2,
+			},
+			false,
+		},
 	}
 
 	for _, testCase := range testcases {
 		t.Run(testCase.name, func(t *testing.T) {
 			err := checkStatusJob(testCase.cmd, testCase.args, testCase.expectStat)
+
+			if (err != nil) != (testCase.expectErr) {
+				t.Errorf("Test case %s with input %s returns error %v, while %v error is expected", testCase.name, testCase.cmd, err, testCase.expectErr)
+				return
+			}
+		})
+	}
+}
+
+func TestStopJob(t *testing.T) {
+	manager, err := job.NewManager()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// checker
+	checkStopJob := func(cmd string, args []string, force bool, expectedStatus *job.ProcStat) error {
+		j, err := manager.CreateJob(cmd, args)
+
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(time.Second)
+
+		if err = j.Stop(force); err != nil {
+			return err
+		}
+
+		status := j.Status()
+
+		if (status.Stat != expectedStatus.Stat) || (status.ExitCode != expectedStatus.ExitCode) {
+			return fmt.Errorf("Status is %v, when expected %v", status, expectedStatus)
+		}
+
+		return err
+	}
+
+	testcases := []struct {
+		name       string
+		cmd        string
+		args       []string
+		forceStop  bool
+		expectStat *job.ProcStat
+		expectErr  bool
+	}{
+		{
+			"Empty Command",
+			"",
+			[]string{},
+			false,
+			&job.ProcStat{
+				Stat:     job.STOPPED,
+				ExitCode: -1,
+			},
+			true,
+		},
+		{
+			"Non exist command",
+			"abc",
+			[]string{},
+			false,
+			&job.ProcStat{
+				Stat:     job.STOPPED,
+				ExitCode: -1,
+			},
+			true,
+		},
+		{
+			"ls",
+			"ls",
+			[]string{"-la"},
+			false,
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 0,
+			},
+			true,
+		},
+		{
+			"mkdir",
+			"mkdir",
+			[]string{fmt.Sprintf("/tmp/%s", randomString(3))},
+			false,
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 0,
+			},
+			true,
+		},
+		{
+			"whoami",
+			"whoami",
+			[]string{},
+			false,
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 0,
+			},
+			true,
+		},
+		{
+			"long running",
+			"top",
+			[]string{"-b"},
+			false,
+			&job.ProcStat{
+				Stat:     job.STOPPED,
+				ExitCode: 0,
+			},
+			false,
+		},
+		{
+			"long running force stop",
+			"top",
+			[]string{"-b"},
+			true,
+			&job.ProcStat{
+				Stat:     job.STOPPED,
+				ExitCode: -1,
+			},
+			false,
+		},
+		{
+			"High priviledge",
+			"apt",
+			[]string{"update"},
+			false,
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 100,
+			},
+			true,
+		},
+		{
+			"sudo",
+			"sudo",
+			[]string{"apt", "update"},
+			false,
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 1,
+			},
+			true,
+		},
+		{
+			"bad args",
+			"ls",
+			[]string{"-wrong"},
+			false,
+			&job.ProcStat{
+				Stat:     job.EXITED,
+				ExitCode: 2,
+			},
+			true,
+		},
+	}
+
+	for _, testCase := range testcases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := checkStopJob(testCase.cmd, testCase.args, testCase.forceStop, testCase.expectStat)
 
 			if (err != nil) != (testCase.expectErr) {
 				t.Errorf("Test case %s with input %s returns error %v, while %v error is expected", testCase.name, testCase.cmd, err, testCase.expectErr)
