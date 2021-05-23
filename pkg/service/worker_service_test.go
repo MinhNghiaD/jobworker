c2d97963599dc9d02e88441798cd23a19f29ba34
Nghia Duong <minhnghia.duong@amadeus.com>
2021-05-23 13:11:21 +0200

    add test request job with wrong id

--- pkg/service/worker_service_test.go
+++ pkg/service/worker_service_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/MinhNghiaD/jobworker/api/client"
 	"github.com/MinhNghiaD/jobworker/api/worker/proto"
 	"github.com/MinhNghiaD/jobworker/pkg/service"
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 )
 
@@ -449,12 +450,63 @@ func TestStopJob(t *testing.T) {
 	for _, testCase := range testcases {
 		t.Run(testCase.name, func(t *testing.T) {
 			err := checkStopJob(testCase.cmd, testCase.args, testCase.forceStop, testCase.expectStat)
-
 			if (err != nil) != (testCase.expectErr) {
 				t.Errorf("Test case %s with input %s returns error %v, while %v error is expected", testCase.name, testCase.cmd, err, testCase.expectErr)
 				return
 			}
 		})
+	}
+}
+
+func TestRequestBadJobs(t *testing.T) {
+	server, err := service.NewServer(7777)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go server.Serve()
+
+	client, err := client.New("127.0.0.1:7777")
+	if err != nil {
+		t.Fatalf("Fail to init client %s", err)
+	}
+
+	defer t.Cleanup(func() {
+		server.Close()
+		client.Close()
+	})
+
+	// Start a job
+	command := &proto.Command{
+		Cmd:  "top",
+		Args: []string{"-b"},
+	}
+
+	job, err := client.Stub.StartJob(context.Background(), command)
+	if err != nil {
+		t.Error(err)
+	}
+
+	status, err := client.Stub.QueryJob(context.Background(), job)
+	if err != nil {
+		t.Error(err)
+	}
+
+	logrus.Infof("Job status %s", status)
+
+	if (status.Status.State != proto.ProcessState_RUNNING) || (status.Status.ExitCode != -1) {
+		t.Error("Status is not as expected")
+	}
+
+	// Query and stop with wrong id
+	_, err = client.Stub.QueryJob(context.Background(), &proto.Job{Id: uuid.New().String()})
+	if err == nil || err.Error() != "Job not found" {
+		t.Error("Wrong error reported")
+	}
+
+	_, err = client.Stub.StopJob(context.Background(), &proto.StopRequest{Job: &proto.Job{Id: uuid.New().String()}, Force: true})
+	if err == nil || err.Error() != "Job not found" {
+		t.Error("Wrong error reported")
 	}
 }
 
